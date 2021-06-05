48ef33be595f14367a088244d4141a01f50f2781
VegetableWithBug <Bensechuangyi17@163.com>
2021-06-05 17:03:48 +0800

    last changed

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -109,6 +109,11 @@ def create_app(test_config=None):
                 return redirect(url_for('login'))
         return wrap
 
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        return redirect(url_for('login'))
+
     
     @app.route('/')
     @login_required
@@ -336,8 +341,7 @@ def create_app(test_config=None):
                 username=text['username']
                 password=text['password']
                 passwordrepeat=text['passwordrepeat']
-                
-                
+                    
                 if(email.isspace() or len(email)==0):
                     error = '邮箱输入不能为空！'
                 elif(username.isspace() or len(username)==0):
@@ -367,7 +371,6 @@ def create_app(test_config=None):
                         x=db.execute(
                             'SELECT id FROM user WHERE username = ?', (username,)
                         ).fetchone()
-                        #print(x['id'])
                         db.execute(
                             'INSERT INTO settings (author_id,setting_name,is_choose,header_L,header_a,header_b,header_magenta ,header_yellow,cyan_standard,magenta_standard,yellow_standard,black_standard,cyan_expend,magenta_expend,yellow_expend,black_expend,cyan_L,cyan_a,cyan_b,magenta_L,magenta_a,magenta_b,yellow_L,yellow_a,yellow_b,black_L,black_a,black_b,header_density_difference,header_difference,middle_expend,four_expend,field_density,field_density_consistency,four_defference,gray_banlance,de_standard,ink_number) VALUES (?,"默认参数",1,50.6,38.5,20.8,0.8,0.8,0.87,0.87,0.85,1.05,17,17,17,17,57,-23,-27,53,48,0,79,-5,60,40,1,4,20,20,20,40,20,0,20,20,0.39,32)',(x['id'],)
                         )
